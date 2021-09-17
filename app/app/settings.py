294c213de13881f66e8b126a13b81e0ef9ac923e
Nick <ion.iascerinschi@gmail.com>
2021-09-17 11:20:07 +0300

    Type in ENGINE

--- app/app/settings.py
+++ app/app/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINGE': 'django.db.backends.postgreql',
+        'ENGINE': 'django.db.backends.postgreql',
         'HOST': os.environ.get('DB_HOST'),
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
