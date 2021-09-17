28beb6d83bb10fe573b7f7f5889fae5bef40fd0d
Nick <ion.iascerinschi@gmail.com>
2021-09-17 20:52:21 +0300

    marius o venit

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,12 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from core import models
+
+def sample_user(email='test@ll.com', password='testpass'):
+    """Create a sample user"""
+    return get_user_model().objects.create_user(email, password)
+
 
 class ModelTests(TestCase):
 
@@ -39,3 +45,14 @@ class ModelTests(TestCase):
         self.assertTrue(user.is_superuser)
         # Our custom field:
         self.assertTrue(user.is_staff)
+
+    def test_history(self):
+        """Test history ... representation ?"""
+        history = models.History.objects.create(
+            user=sample_user(),
+            name='TestUserHistory'
+        )
+
+        # TODO: Test that history works properly [Nick]
+        # history.id
+        self.assertTrue(True)
