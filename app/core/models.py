63a9d5c611c8ba4dfc94ca3ecd95fcc4b821d68e
Nick <ion.iascerinschi@gmail.com>
2021-09-23 12:39:46 +0300

    Solved linting issues

--- app/core/models.py
+++ app/core/models.py
@@ -39,6 +39,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     USERNAME_FIELD = 'email'
 
+
 class Tag(models.Model):
     """Tag to be used for a recipe"""
     name = models.CharField(max_length=255)
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,8 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
-from core import models
-
 def sample_user(email='test@ll.com', password='testpass'):
     """Create a sample user"""
     return get_user_model().objects.create_user(email, password)
